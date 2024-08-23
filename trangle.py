8cbcaeb23a7e2dea8c362c61022c43886029a342
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-08-23 07:54:45 +0530

    Add files via upload

---
+++ trangle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 12:36:41 2024
+
+@author: ADMIN
+"""
+
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
