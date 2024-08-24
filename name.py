23aad6177dfe5f7e6b87db1b8002b4bfc6bed7fb
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-08-25 02:05:34 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+turtle.color("yellow")
+style = ("algerian",'30','bold','italic','underline')
+turtle.write(" SUTIKSHAN YADAV ", font = style,align = 'center')
+turtle.hideturtle()
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Font")
+
+
+
+ 
