7584760de7571c9d4035b1f97996afbe1ae8fa2b
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-08-25 19:47:01 +0530

    Add files via upload

---
+++ hexagone.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+t=turtle.Turtle()
+t.pensize(10)
+t.color("blue")
+t.penup()
+t.goto(-250,100)
+t.pendown()
+#letter T
+
+t.forward(200)
+t.back(100)
+t.right(90)
+t.forward(200)
+#move
+
+t.penup()
+t.back(200)
+t.left(90)
+t.forward(150)
+t.pendown()
+t.color("yellow")
+
+
+
+ 
