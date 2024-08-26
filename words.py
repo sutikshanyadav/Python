56890e7dbc2af0a7cfea8c9b35d25cac54125e3a
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-08-26 15:55:16 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+t=turtle.Turtle()
+t.pensize(10)
+t.color("blue")
+t.penup()
+t.goto(-250,100)
+t.pendown()
+#letter
+t.color("black")
+t.forward(200)
+t.back(100)
+t.right(90)
+t.forward(200)
+#move
+
+t.penup()
+t.back(200)
+t.left(90)
+t.forward(150)
+t.pendown()
+t.color("yellow")
+
+#m
+t.right(90)
+t.forward(200)
+t.back(200)
+t.left(30)
+t.forward(150)
+t.left(120)
+t.forward(150)
+t.right(150)
+t.forward(200)
+
+
+
+
+
+
+ 
