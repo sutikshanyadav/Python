28f902aec24dad5d88d8041d7831ca7d5488113d
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-08-29 02:34:07 +0530

    Add files via upload

---
+++ home.circ.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+t=turtle.Turtle()
+t.speed(100)
+for i in range(1000):
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.right(90)
+    t.forward(30)
+    t.left(120)
+    t.forward(50)
+    t.left(60)
+    t.forward(200)
+    t.left(60)
+    t.forward(50)
+    t.left(120)
+    t.forward(230)
+    t.left(180)
+    t.penup()
+    t.forward(210)
+    t.pendown()
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(80)
+    t.left(90)
+    t.forward(60)
+    t.right(90)
+    t.forward(40)
+    t.right(90)
+    t.forward(60)
+    t.left(90)
+    t.left(67)
+
+
+
+
+
+
+ 
