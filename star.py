550352a0e16364468200337fc04de70cc8bd32f3
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-08-30 13:55:37 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle
+t=turtle.Turtle()
+t.speed(100)
+t.color("red")
+t.left(45)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.right(135)
+t.forward(100)
+t.left(70)
+t.forward(100)
+t.right(145)
+t.forward(130)
+t.left(70)
+t.forward(130)
+t.right(140)
+t.forward(100)
+t.left(70)
+t.forward(100)
+t.right(127)
+t.forward(82)
+t.hideturtle()
+
+
+        
+
+
+
+
+
+
+ 
