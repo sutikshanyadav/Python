87f124bc22244c3a19b98e6d415b392648abc4f5
sutikshanyadav <117886806+sutikshanyadav@users.noreply.github.com>
2024-09-19 21:47:21 +0530

    Add files via upload

---
+++ multpn of n no..py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 12:30:25 2024
+
+@author: ADMIN
+"""
+
+n = 100
+multiply = 1
+
+for i in range(1, n + 1):
+    multiply *= i
+
+print(f"multiplication is: {multiply}")
